f4f56cf5ec813cdcb7a4f5c33aa401241ba38f02
jhyom <jaehongyom@gmail.com>
2023-04-24 13:01:05 +0900

    fixed for flake8

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -4,6 +4,7 @@ from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
@@ -13,7 +14,7 @@ class Command(BaseCommand):
         while db_up is False:
             try:
                 self.check(databases=['default'])
-                db_up =True
+                db_up = True
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Databse uavailabe, waiting 1 second...')
                 time.sleep(1)
